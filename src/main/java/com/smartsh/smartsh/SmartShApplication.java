ba64cb2da2318bed57f91ad50e8e8dd7b36e3e5a
Maurelio <marcosaureliomendesoliveira@gmail.com>
2024-06-03 21:20:56 -0300

    [CONF] - Initial commit

---
+++ src/main/java/com/smartsh/smartsh/SmartShApplication.java
@@ -0,0 +1,13 @@
+package com.smartsh.smartsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartsh/smartsh/SmartShApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartsh.smartsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
